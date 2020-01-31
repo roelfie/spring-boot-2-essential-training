b538c66508ad81d6500a4bc7e5730fd4256d37f3
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-31 23:20:16 +0100

    Spring Boot web project generated by Spring Initializr, with actuator and thymeleaf.

---
+++ src/main/java/top/kerstholt/springboot/SpringBoot2EssentialsApplication.java
@@ -0,0 +1,13 @@
+package top.kerstholt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2EssentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/kerstholt/springboot/SpringBoot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kerstholt.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
